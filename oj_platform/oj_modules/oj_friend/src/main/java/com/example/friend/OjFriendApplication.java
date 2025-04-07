b1b808d6fee9fde522ba1da64621e874d7b704cf
zhangyc295 <zhangyc295@outlook.com>
2025-04-07 22:25:12 +0800

    后端工程第一次创建

---
+++ oj_platform/oj_modules/oj_friend/src/main/java/com/example/friend/OjFriendApplication.java
@@ -0,0 +1,11 @@
+package com.example.friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjFriendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjFriendApplication.class, args);
+    }
+}
